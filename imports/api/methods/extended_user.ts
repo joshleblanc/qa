6b540d2335f2a1e6e6a0500c207433e463ad1dc5
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-01-29 20:11:57 -0400

    remove unneccessary mutateUser function

--- imports/api/methods/extended_user.ts
+++ imports/api/methods/extended_user.ts
@@ -4,11 +4,6 @@ export interface ExtendedUser extends Meteor.User {
   admin?: boolean;
 }
 
-export function mutateUser(user: Meteor.User|null): ExtendedUser|null {
-  return user;
-}
-
-export function isAdmin(user: Meteor.User|null): boolean {
-  const mutatedUser = mutateUser(user);
-  return Boolean(mutatedUser?.admin);
+export function isAdmin(user: ExtendedUser|null): boolean {
+  return Boolean(user?.admin);
 }
